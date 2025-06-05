46a406d1b7929188ed8833fce299c65c0e8661b1
Priyansh2603 <ishudaksh2603@gmail.com>
2025-06-05 11:01:38 +0530

    fetch

--- src/api/satelite.js
+++ src/api/satelite.js
@@ -1,26 +1,19 @@
 import axios from "axios";
+
 function formatAndIncrementDate(dateString) {
   const date = new Date(dateString);
-  date.setDate(date.getDate() ); // Increment by 1 day
-
-  // Format as YYYY-MM-DD
-  const formatted = date.toISOString().split("T")[0];
-  return formatted;
+  date.setDate(date.getDate());
+  return date.toISOString().split("T")[0];
 }
 
-export const fetchNDMI = async (geometry,sowingDate) => {
-  const proxyUrl = "https://corsproxy.io/?";
-  const targetUrl = "https://server.cropgenapp.com/get-vegetation-index";
-
-  const res = await axios.post(proxyUrl + targetUrl,
-    {
-      start_date: "2025-06-03",
-      end_date: formatAndIncrementDate(new Date()),
-      geometry,
-      index: "NDMI",
-      dataset: "COPERNICUS/S2_HARMONIZED"
-    }
-  );
+export const fetchNDMI = async (geometry, sowingDate) => {
+  const res = await axios.post("https://map-backend-wz2x.vercel.app/proxy/ndmi", {
+    start_date: formatAndIncrementDate(new Date(sowingDate)),
+    end_date: formatAndIncrementDate(new Date()),
+    geometry,
+    index: "NDMI",
+    dataset: "COPERNICUS/S2_HARMONIZED"
+  });
 
   return res.data;
 };
