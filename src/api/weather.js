68013f78b65a263917bdd50cfd6e36d165c4ca7c
Priyansh2603 <ishudaksh2603@gmail.com>
2025-06-06 19:24:25 +0530

    weather

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
+import UserAxiosAPI from "./userAxiosAPI";
 
 export const fetchWeather = async (lat, lon) => {
-  const res = await axios.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
-  );
-  return res.data.current_weather;
+  const axios = UserAxiosAPI();
+  const res = await axios.post("https://map-backend-wz2x.vercel.app/proxy/weather", {
+    lat,
+    lon,
+  });
+  return res.data;
 };
